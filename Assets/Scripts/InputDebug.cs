a51dc50c2154c6c00b0afadb958321a362935fbc
Julia Costa <juliacosta232005@gmail.com>
2025-01-25 17:42:10 +0000

    Finish to Setup Input Manager Controlls

--- Assets/Scripts/InputDebug.cs
+++ Assets/Scripts/InputDebug.cs
@@ -5,6 +5,8 @@ public class InputDebug : MonoBehaviour
     [SerializeField] private string _playerNumber;
     private string _verticalAxis;
     private string _horizontalAxis;
+    private string _aimXAxis;
+    private string _aimYAxis;
     private string _fireButton;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -12,12 +14,17 @@ public class InputDebug : MonoBehaviour
     {   
         _verticalAxis   = "MoveY" + _playerNumber;
         _horizontalAxis = "MoveX" + _playerNumber;
+        _aimYAxis       = "AimY" + _playerNumber;
+        _aimXAxis       = "AimX" + _playerNumber;
         _fireButton     = "Fire" + _playerNumber;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log($"Player {_playerNumber} Horizontal : {Input.GetAxis(_horizontalAxis)} Vertical : {Input.GetAxis(_verticalAxis)} Fire : {Input.GetButton(_fireButton)}");
+        Debug.Log($"Player {_playerNumber}");
+        Debug.Log($"Move\nHorizontal : {Input.GetAxis(_horizontalAxis)} Vertical : {Input.GetAxis(_verticalAxis)}");
+        Debug.Log($"Aim\nHorizontal : {Input.GetAxis(_aimXAxis)} Vertical : {Input.GetAxis(_aimYAxis)}");
+        Debug.Log($"Fire : {Input.GetButton(_fireButton)}");
     }
 }
